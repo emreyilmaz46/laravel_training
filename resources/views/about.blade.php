eea72975d6c7d3067c9e241bfbb2455fa2845d77
Abdussamet ASLAN <aaslan@sibermen.com>
2017-06-18 11:46:54 +0000

    make auth & about controller view added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title') About @endsection
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">About</div>
+
+                    <div class="panel-body">
+                        Welcome back!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
